e4103124d3fb2a3ca15e7d122bb6db92450d8653
UTSAHSINGH <152717877+UTSAHSINGH@users.noreply.github.com>
2026-02-04 10:12:59 +0530

    Time: 0 ms (100%), Space: 11.3 MB (79.41%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    int heightChecker(vector<int>& arr) {
-        vector <int> copy=arr;
-        sort(copy.begin(),copy.end());
-        int c=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]!=copy[i]){
-                c++;
-            }
-        }return c;
-
-        
+    int heightChecker(std::vector<int>& heights) {
+        std::vector<int> expected(heights);
+        std::sort(expected.begin(), expected.end());
+        int count=0;
+        for (int i = 0; i < heights.size(); i++)
+        {
+            if(heights[i]!=expected[i]) count++;
+        }
+        return count;
     }
 };
